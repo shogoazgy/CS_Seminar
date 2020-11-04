2ca296feff16de1c88291cdbf162159142602160
成田省吾 <shougo@naritashougonoMacBook-Pro.local>
2020-11-04 17:26:51 +0900

    first commit

---
+++ chat/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 8000;
+
+app.get('/' , function(req, res){
+    res.sendFile(__dirname+'/index.html');
+
+});
+
+io.on('connection',function(socket){
+    socket.on('message',function(msg){
+        console.log('message: '+msg);
+        io.emit('message',msg);
+    });
+});
+
+http.listen(PORT, function(){
+    console.log('server listenning. Port:'+PORT);
+});
